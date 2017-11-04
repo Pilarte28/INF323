4acfd5e0146c2f720fa788f209a0dd2a1bc49484
Fernando N. <fernan2pilarte@gmail.com>
2017-11-04 09:10:25 -0400

    https://github.com/Pilarte28/INF323.git

--- WFA001/Form1.Designer.cs
+++ WFA001/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 324);
+            this.ClientSize = new System.Drawing.Size(828, 376);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
